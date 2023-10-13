3dec93722558e73e7ade93cba38cd690acea1255
Crazie-ash <m.avinashkumar96@gmail.com>
2023-10-13 11:51:25 +0530

    change the routes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -109,13 +109,7 @@ function Footer() {
           <NavLink href="#education">Education</NavLink>
         </Nav>
         <SocialMediaIcons>
-<<<<<<< HEAD
-          {/* <SocialMediaIcon href={Bio.facebook} target="display"><FacebookIcon /></SocialMediaIcon> */}
-          <SocialMediaIcon href={Bio.whatsapp} target="display">< WhatsAppIcon/></SocialMediaIcon>
-          <SocialMediaIcon href={Bio.linkedin} target="display"><LinkedInIcon /></SocialMediaIcon>
-          <SocialMediaIcon href={Bio.insta} target="display"><InstagramIcon /></SocialMediaIcon>
-          <SocialMediaIcon href={Bio.youtube} target="display"><YouTubeIcon /></SocialMediaIcon>
-=======
+
           <SocialMediaIcon href={Bio.whatsapp} target="_blank">
             <WhatsAppIcon />
           </SocialMediaIcon>
@@ -125,7 +119,7 @@ function Footer() {
           <SocialMediaIcon href={Bio.insta} target="_blank">
             <InstagramIcon />
           </SocialMediaIcon>
->>>>>>> 5aab9f5b4e8dc600b130468f69bb968950a2c1ef
+
         </SocialMediaIcons>
         <Copyright>
           &copy; {currentYear} Avinash. All rights reserved.
