29da4e0c07789a084e6a9f6357cd61adecfd891f
Crazie-ash <m.avinashkumar96@gmail.com>
2023-10-18 23:06:05 +0530

    connect to Gsheet

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -183,7 +183,7 @@ const Contact = () => {
         </ContactForm>
         <Snackbar
           open={open}
-          autoHideDuration={5000}
+          autoHideDuration={4000}
           onClose={() => setOpen(false)}
           message="Enquiry added successfully!"
           severity="success"
