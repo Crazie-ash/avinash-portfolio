72042eb377ae030f73cb92e892d42621e6442656
Crazie-ash <m.avinashkumar96@gmail.com>
2023-11-01 16:25:49 +0530

    comment the WB button

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -29,7 +29,7 @@ const HeroSection = () => {
                         </TextLoop>
                         <SubTitle>{Bio.description}</SubTitle>
                         <ResumeButton href={Bio.resume} target='display'>Check Resume</ResumeButton>
-                        <WhatsappButton href={Bio.whatsapp} target='display'>Click</WhatsappButton>
+                        {/* <WhatsappButton href={Bio.whatsapp} target='display'>Click</WhatsappButton> */}
                     </HeroLeftContainer>
 
                     <HeroRightContainer id="Right">
